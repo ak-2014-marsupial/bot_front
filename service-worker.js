8a4747a134b0bfeab314ca953000e50940209506
Andrew <ak36794ca@gmail.com>
2024-08-14 15:17:09 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,27 @@
-const staticCacheName="static-cache-v0";
+const staticCacheName = "static-cache-v0";
 
-const staticAssets=[
-
+const staticAssets = [
+    "./logo192.png",
+    "./favicon.ico",
+    "./telegram-web-app.js",
+    "./static/js/main.56b6cfb5.js",
+    "./static/css/main.1e39e86c.css"
 ]
 
-self.addEventListener("install", async event=>{
+self.addEventListener("install", async event => {
+    const cache = await caches.open(staticCacheName);
+    await  cache.addAll(staticAssets);
     console.log("Service worker has been installed");
 })
 
-self.addEventListener("activate", async event=>{
+self.addEventListener("activate", async event => {
     console.log("Service worker has been activated");
+
 })
 
-self.addEventListener("fetch", event =>{
+self.addEventListener("fetch", event => {
     console.log(`Trying to fetch ${event.request.url}`);
-} )
+    event.respondWith(caches.match(event.request).then(cachedResponse=>{
+        return cachedResponse || fetch(event.request)
+    }))
+})
